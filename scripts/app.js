70336a9f2ba122cdbc996b2f81ff54a45a2da482
Georges Creates <georgescreates@gmail.com>
2022-11-20 18:52:46 -0500

    created scripts folder and added app.js file

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+let surveyForm = document.getElementById('survey-form');
+
+surveyForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    //surveyChoiceValidatorBtn.textContent = "Merci pour vos choix.";
+})
